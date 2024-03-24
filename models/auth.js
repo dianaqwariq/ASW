751276092aefdcef73f73ccbac6353d9fc2d85d0
anwar_aqraa <anwaraqraa932@gmail.com>
2024-03-24 13:17:08 +0200

    Change

--- models/auth.js
+++ models/auth.js
@@ -5,10 +5,10 @@ const authenticateTokenHandler = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if (authHeader) {
-        const token = authHeader.split(' ')[1]; 
+        const token = authHeader.split(' ')[1];
         jwt.verify(token, process.env.SECRET_KEY || 'fallbackSecret', (err, user) => {
             if (err) {
-                console.error(err); 
+                console.error(err);
                 return res.sendStatus(403);
             }
             req.user = user;
@@ -20,7 +20,7 @@ const authenticateTokenHandler = (req, res, next) => {
 };
 
 const generateToken = (user) => {
-    return jwt.sign(user, process.env.SECRET_KEY || 'fallbackSecret', { expiresIn: '1h' });
+    return jwt.sign(user, process.env.SECRET_KEY || 'fallbackSecret', { expiresIn: '24h' });
 };
 
 module.exports = {
