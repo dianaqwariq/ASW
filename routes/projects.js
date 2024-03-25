aed61a37f13787ceef962cb205c8ff1552bfe159
anwar_aqraa <anwaraqraa932@gmail.com>
2024-03-25 23:11:30 +0200

    change_name

--- routes/projects.js
+++ routes/projects.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const projectController = require('../controllers/projectController');
+const projectController = require('../controllers/projectController1');
 
 
 router.get('/getAllProjects', projectController.getAllProjects);
